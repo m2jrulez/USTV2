71e0c2442f080ce754b55ce41fe8c1a99a1fb8c9
Ezzeldien Azhari <masterezzel@gmail.com>
2016-09-18 17:19:35 +0300

    Update

--- USTV2/GUI/Registrar/frmNewStudent.Designer.cs
+++ USTV2/GUI/Registrar/frmNewStudent.Designer.cs
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // frmNewStudent
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1008, 729);
+            this.Name = "frmNewStudent";
             this.Text = "frmNewStudent";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
